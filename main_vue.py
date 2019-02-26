53b72107680b87543949c7bd546805b3bf2be530
Quentin Nadé <quentin.nade27@gmail.com>
2019-02-26 15:13:06 +0100

    Suppresion des imports inutiles

--- main_vue.py
+++ main_vue.py
@@ -1,15 +1,4 @@
-from model.couleurCarte import *
-from model.valeurCarte import *
-from model.carte import *
-from model.joueur import *
-from model.game import *
 from vue.cadre import *
-from vue.screen import *
-from vue.screenManager import *
-from vue.gameScreen import *
-from vue.menuScreen import *
-from controller.controller import *
-from vue.utilitaire import *
 
 app = Cadre()
 app.mainloop()
