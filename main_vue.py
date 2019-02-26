29902604a9c76445257d292ad5d6a2117e0a1f65
Quentin Nadé <quentin.nade27@gmail.com>
2019-02-26 14:43:15 +0100

    update

--- main_vue.py
+++ main_vue.py
@@ -11,45 +11,7 @@ from vue.menuScreen import *
 from controller.controller import *
 from vue.utilitaire import *
 
-j1 = Joueur("j1")
-j2 = Joueur("j2")
-
-g = Game()
-
-g.addJoueur(j1)
-g.addJoueur(j2)
-
-g.distribuerCartes()
-
-print("Nombre de joueurs : " + str(len(g.listeJoueurs)))
-
-print("---------------------------------")
-
-print("Liste des joueurs :")
-g.printListeJoueurs()
-
-j1.addCarte(Carte(CouleurCarte.CARREAU, ValeurCarte.ROI))
-j1.addCarte(Carte(CouleurCarte.COEUR, ValeurCarte.ROI))
-j1.addCarte(Carte(CouleurCarte.COEUR, ValeurCarte.DEUX))
-
-j2.addCarte(Carte(CouleurCarte.CARREAU, ValeurCarte.TROIS))
-
-# Partie graphique
-
 root = Tk()
-
-# screenManager = ScreenManager()
-cadre = Cadre(root, 900, 900, 'yellow')
-# cadre = Cadre(screenManager, 800, 600)
-
-gameScreen = GameScreen(cadre, 600, 600, 'green')
-menuScreen = MenuScreen(cadre, 600, 600)
-cadre.getScreenManager().ajouterScreen(gameScreen.getType(), gameScreen)
-cadre.getScreenManager().ajouterScreen(menuScreen.getType(), menuScreen)
-
-cadre.getScreenManager().setScreenCourant(gameScreen.getType())
-
-controller = Controller(cadre, g)
-
-cadre.affiche()
-cadre.mainloop()
+root.withdraw()
+app = Controller(root);
+root.mainloop()
